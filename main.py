7ba8f42da7b67314c35edf10b7d8d03db02b1174
Mason Jennings <masonj@csu.fullerton.edu>
2025-02-04 22:34:24 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Instructor: Edwin D. Sookiassian
+CSIS 151 - Assignment 5: Class Creation
+Name: Mason Jennings
+Date: 2-5-2025
+"""
+
+def main():
+    print("Hello world.")
+
+if __name__ == "__main__":
+    main()
