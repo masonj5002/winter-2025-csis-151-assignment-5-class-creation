2639848c771f43b595af16c9a9b89d8c4b377bb4
Mason Jennings <masonj@csu.fullerton.edu>
2025-02-07 01:36:26 -0800

    added assessors

--- library_book_inventory.py
+++ library_book_inventory.py
@@ -6,4 +6,23 @@ Date: 2-7-2025
 """
 
 class LibraryBookInventory:
-    pass
+    def __init__(self, title, author, publication_year, copies_available = 0,
+                 copies_checked_out = 0) -> None:
+        self._title = title
+        self._author = author
+        self._publication_year = publication_year
+        self._copies_available = copies_available
+
+    def get_title(self):
+        return self._title
+    
+    def get_author(self):
+        return self._author
+    
+    def get_publication_year(self):
+        return self._publication_year
+    
+    def get_copies_available(self):
+        return self._copies_available
+    
+    #def 
